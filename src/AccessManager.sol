dde1041ac2230cbd561235423832fd37d4400a26
frwd-1 <frwdlabs@gmail.com>
2024-01-23 17:46:08 -0700

    Update AccessManager.sol

--- src/AccessManager.sol
+++ src/AccessManager.sol
@@ -5,6 +5,7 @@ import "./interfaces/IAccessManager.sol";
 import "./dao/interfaces/IDAO.sol";
 import "./SigningTools.sol";
 
+// test
 
 // A simple AccessManager in which user IP is mapped offchain to a geolocation and then whitelisted user status is stored in the contract.
 // If geographic regions are later excluded, users are required to reverify (allowing avoiding storing countries for specific wallets and potentially violating user privacy).
@@ -16,64 +17,63 @@ import "./SigningTools.sol";
 //
 // Updateable using DAO.proposeSetContractAddress( "accessManager" )
 
-contract AccessManager is IAccessManager
-	{
-	event AccessGranted(address indexed wallet, uint256 geoVersion);
+contract AccessManager is IAccessManager {
+    event AccessGranted(address indexed wallet, uint256 geoVersion);
 
-	IDAO immutable public dao;
+    IDAO public immutable dao;
 
-	// Determines granted access for [geoVersion][wallet]
+    // Determines granted access for [geoVersion][wallet]
     mapping(uint256 => mapping(address => bool)) private _walletsWithAccess;
 
-	// The current geoVersion for the AccessManager - which is incremented when new countries are excluded by the DAO.
+    // The current geoVersion for the AccessManager - which is incremented when new countries are excluded by the DAO.
     uint256 public geoVersion;
 
+    constructor(IDAO _dao) {
+        dao = _dao;
+    }
 
-	constructor( IDAO _dao )
-		{
-		dao = _dao;
-		}
-
-
-	// Called whenever the DAO updates the list of excluded countries.
-	// This effectively clears access for all users as the geoVersion is used to reference _walletsWithAccess.
-	// If, in contrast, new countries are included, then updating the geoVersion isn't necessary as the existing _walletsWithAccess will still be valid.
-    function excludedCountriesUpdated() external
-    	{
-    	require( msg.sender == address(dao), "AccessManager.excludedCountriedUpdated only callable by the DAO" );
+    // Called whenever the DAO updates the list of excluded countries.
+    // This effectively clears access for all users as the geoVersion is used to reference _walletsWithAccess.
+    // If, in contrast, new countries are included, then updating the geoVersion isn't necessary as the existing _walletsWithAccess will still be valid.
+    function excludedCountriesUpdated() external {
+        require(
+            msg.sender == address(dao),
+            "AccessManager.excludedCountriedUpdated only callable by the DAO"
+        );
 
         geoVersion += 1;
-    	}
+    }
 
+    // Verify that the access request was signed by the authoratative signer.
+    // Note that this is only a simplistic default mechanism and can be changed by the DAO at any time (either altering the regional restrictions themselves or replacing the access mechanism entirely).
+    function _verifyAccess(
+        address wallet,
+        bytes memory signature
+    ) internal view returns (bool) {
+        bytes32 messageHash = keccak256(
+            abi.encodePacked(block.chainid, geoVersion, wallet)
+        );
 
-	// Verify that the access request was signed by the authoratative signer.
-	// Note that this is only a simplistic default mechanism and can be changed by the DAO at any time (either altering the regional restrictions themselves or replacing the access mechanism entirely).
-    function _verifyAccess(address wallet, bytes memory signature ) internal view returns (bool)
-    	{
-		bytes32 messageHash = keccak256(abi.encodePacked(block.chainid, geoVersion, wallet));
+        return SigningTools._verifySignature(messageHash, signature);
+    }
 
-		return SigningTools._verifySignature(messageHash, signature);
-    	}
-
-
-	// Grant access to the sender for the given geoVersion.
-	// Requires the accompanying correct message signature from the offchain verifier.
-    function grantAccess(bytes calldata signature) external
-    	{
-    	require( _verifyAccess(msg.sender, signature), "Incorrect AccessManager.grantAccess signatory" );
+    // Grant access to the sender for the given geoVersion.
+    // Requires the accompanying correct message signature from the offchain verifier.
+    function grantAccess(bytes calldata signature) external {
+        require(
+            _verifyAccess(msg.sender, signature),
+            "Incorrect AccessManager.grantAccess signatory"
+        );
 
         _walletsWithAccess[geoVersion][msg.sender] = true;
 
-        emit AccessGranted( msg.sender, geoVersion );
-    	}
+        emit AccessGranted(msg.sender, geoVersion);
+    }
 
+    // === VIEWS ===
 
-	// === VIEWS ===
-
-	// Returns true if the wallet has access at the current geoVersion
-    function walletHasAccess(address wallet) external view returns (bool)
-    	{
+    // Returns true if the wallet has access at the current geoVersion
+    function walletHasAccess(address wallet) external view returns (bool) {
         return _walletsWithAccess[geoVersion][wallet];
-    	}
+    }
 }
-
